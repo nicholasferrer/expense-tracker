471747a76735b5bcd3e994f91c0a31ec8efd37fc
Nicholas Ferrer <nferrer@americastg.com>
2024-07-17 22:45:40 -0300

    calc summary and format values

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -7,6 +7,8 @@ import { SearchForm } from "./components/SearchForm";
 
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
+
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext);
 
@@ -26,11 +28,12 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
